480dcbe2d0ad3446685ef88d8b11c6ccf3c9e0fb
Christopher Groskopf <staringmonkey@gmail.com>
2013-02-28 09:29:21 -0500

    Generalize upload exception handlers.

--- upload_app.py
+++ upload_app.py
@@ -10,7 +10,7 @@ from boto.s3.key import Key
 
 from flask import Flask, redirect
 from tumblpy import Tumblpy
-from tumblpy import TumblpyRateLimitError, TumblpyAuthError
+from tumblpy import TumblpyError
 from werkzeug import secure_filename
 
 import app_config
@@ -53,77 +53,66 @@ def _post_to_tumblr():
         value = re.sub(r'\r\n|\r|\n', '\n', value)
         return value.replace('\n', '<br />')
 
-    caption = u"""
-        <p class='message'>%s</p>
-        <p class='signature-name'>Initialed,<br/>%s from %s</p>
-        <p class='footnote'>Dinner is hard. We want to know what's on your family's table, and why.
-        Share yours at <a href='http://%s/'>NPR's Dinnertime Confessional</a>.</p>
-    """ % (
-        strip_breaks(strip_html(request.form['message'])),
-        strip_html(request.form['signed_name']),
-        strip_html(request.form['location']),
-        app_config.TUMBLR_URL
-    )
-
-    filename = secure_filename(request.files['image'].filename.replace(' ', '-'))
-
-    conn = boto.connect_s3()
-    bucket = conn.get_bucket(app_config.S3_BUCKETS[0])
-    headers = {
-        'Content-Type': request.files['image'].content_type,
-        'Cache-Control': 'public, max-age=31536000'
-    }
-    policy = 'public-read'
-
-    k = Key(bucket)
-    k.key = '%s/tmp/%s' % (app_config.DEPLOYED_NAME, filename)
-    k.set_contents_from_file(request.files['image'].stream, headers=headers, policy=policy, rewind=True)
-
-    t = Tumblpy(
-        app_key=app_config.TUMBLR_KEY,
-        app_secret=os.environ['TUMBLR_APP_SECRET'],
-        oauth_token=os.environ['TUMBLR_OAUTH_TOKEN'],
-        oauth_token_secret=os.environ['TUMBLR_OAUTH_TOKEN_SECRET'])
-
-    s3_path = 'http://%s.s3.amazonaws.com/%s/tmp/%s' % (
-            app_config.S3_BUCKETS[0],
-            app_config.DEPLOYED_NAME,
-            filename
+    try:
+        caption = u"""
+            <p class='message'>%s</p>
+            <p class='signature-name'>Initialed,<br/>%s from %s</p>
+            <p class='footnote'>Dinner is hard. We want to know what's on your family's table, and why.
+            Share yours at <a href='http://%s/'>NPR's Dinnertime Confessional</a>.</p>
+        """ % (
+            strip_breaks(strip_html(request.form['message'])),
+            strip_html(request.form['signed_name']),
+            strip_html(request.form['location']),
+            app_config.TUMBLR_URL
         )
 
-    params = {
-        'type': 'photo',
-        'caption': caption,
-        'tags': u"food,dinner,plate,confession,crunchtime,npr",
-        'source': s3_path
-    }
+        filename = secure_filename(request.files['image'].filename.replace(' ', '-'))
 
-    tumblr_dict = {}
-    tumblr_dict['timestamp'] = datetime.datetime.now()
+        conn = boto.connect_s3()
+        bucket = conn.get_bucket(app_config.S3_BUCKETS[0])
+        headers = {
+            'Content-Type': request.files['image'].content_type,
+            'Cache-Control': 'public, max-age=31536000'
+        }
+        policy = 'public-read'
 
-    try:
-        tumblr_post = t.post('post', blog_url=app_config.TUMBLR_URL, params=params)
-        tumblr_dict['tumblr_id'] = tumblr_post['id']
-        tumblr_dict['tumblr_url'] = u"http://%s/%s" % (app_config.TUMBLR_URL, tumblr_post['id'])
-        tumblr_dict['result'] = {'code': 200, 'message': 'success'}
-        logger.info('200 %s' % tumblr_dict['tumblr_url'])
+        k = Key(bucket)
+        k.key = '%s/tmp/%s' % (app_config.DEPLOYED_NAME, filename)
+        k.set_contents_from_file(request.files['image'].stream, headers=headers, policy=policy, rewind=True)
 
-        return redirect(u"http://%s/%s#posts" % (app_config.TUMBLR_URL, tumblr_post['id']), code=301)
+        t = Tumblpy(
+            app_key=app_config.TUMBLR_KEY,
+            app_secret=os.environ['TUMBLR_APP_SECRET'],
+            oauth_token=os.environ['TUMBLR_OAUTH_TOKEN'],
+            oauth_token_secret=os.environ['TUMBLR_OAUTH_TOKEN_SECRET'])
 
-    except TumblpyAuthError:
-        tumblr_dict['result'] = {'code': 401, 'message': 'Failed: Not authenticated.'}
-        logger.error('401 Not Authenticated')
-        return 'NOT AUTHENTICATED'
+        s3_path = 'http://%s.s3.amazonaws.com/%s/tmp/%s' % (
+                app_config.S3_BUCKETS[0],
+                app_config.DEPLOYED_NAME,
+                filename
+            )
 
-    except TumblpyRateLimitError:
-        tumblr_dict['result'] = {'code': 403, 'message': 'Failed: Rate limited.'}
-        logger.error('403 Rate Limited')
-        return 'RATE LIMITED'
+        params = {
+            'type': 'photo',
+            'caption': caption,
+            'tags': u"food,dinner,plate,confession,crunchtime,npr",
+            'source': s3_path
+        }
 
-    except:
-        tumblr_dict['result'] = {'code': 400, 'message': 'Failed: Unknown.'}
-        logger.error('400 Unknown')
-        return 'UNKNOWN FAILURE'
+        try:
+            tumblr_post = t.post('post', blog_url=app_config.TUMBLR_URL, params=params)
+            tumblr_url = u"http://%s/%s" % (app_config.TUMBLR_URL, tumblr_post['id'])
+            logger.info('200 %s' % tumblr_url)
+
+            return redirect(u"http://%s/%s#posts" % (app_config.TUMBLR_URL, tumblr_post['id']), code=301)
+
+        except TumblpyError, e:
+            logger.error('%s %s' % (e.error_code, e.msg))
+            return 'TUMBLR ERROR'
+
+    except Exception, e:
+        logger.error('%s' % e)
+        return 'ERROR'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001, debug=app_config.DEBUG)
